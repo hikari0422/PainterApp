41597e4a3d541323a13a59fe3f88395592f16e40
hikari <ben109092ben@gmail.com>
2025-10-15 11:11:56 +0800

    調整視窗屬性並新增繪圖功能與資源
    更新了視窗大小與畫布屬性，新增筆刷粗細滑桿與多種形狀選擇按鈕，並實作相關事件處理程序。
    新增狀態列以顯示當前狀態、顏色與形狀資訊。
    新增 `line.png`、`rectangle.png` 等圖片資源，並更新專案檔以包含必要的套件與資源設定。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,58 @@ namespace PainterApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        Point start = new Point { X = 0, Y = 0 };
+        Point end = new Point { X = 0, Y = 0 };
+
+        Color strokeColor = Colors.Black;
+        Color fillColor = Colors.Transparent;
+
+        int strokeThiness = 1;
+
+        string shapeType = "Line";
+
         public MainWindow()
         {
             InitializeComponent();
+            strokeColorPicker.SelectedColor = strokeColor;
+            fillColorPicker.SelectedColor = fillColor;
+        }
+
+        private void ShapeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void strokeColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            strokeColor = strokeColorPicker.SelectedColor.Value;
+            DisplayStatus();
+        }
+
+        private void DisplayStatus()
+        {
+            if (statusLabel != null) statusLabel.Content = $"Start:({start.X},{start.Y}) End:({end.X},{end.Y}";
+            if (colorLabel != null) colorLabel.Content = $"筆刷色彩: {strokeColor} 填滿色彩: {fillColor} 筆刷粗細: {strokeThiness}";
+            if (shapeLabel != null) shapeLabel.Content = $"形狀: {shapeType}";
+        }
+
+        private void fillColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fillColor = fillColorPicker.SelectedColor.Value;
+            DisplayStatus();
+        }
+
+        private void ShapeButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var targetRadioButton = sender as RadioButton;
+            shapeType = targetRadioButton.Tag.ToString();
+            DisplayStatus();
+        }
+
+        private void strokeThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            strokeThiness = (int)strokeThicknessSlider.Value;
+            DisplayStatus();
         }
     }
 }
